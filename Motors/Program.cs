ef123a40e6d7d2c98718a16c3fafff576a454782
Kristóf Borbára <borbara.kristof@petrik.hu>
2025-03-14 12:38:34 +0100

    Classok És program.cs

--- Motors/Program.cs
+++ Motors/Program.cs
@@ -4,7 +4,14 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Statisztika statisztika = new Statisztika();
+            statisztika.ReadFromFile("motors.txt");
+            Console.WriteLine("Az összes motor ára egyben: " +statisztika.SumPrices() + "EUR");
+            Console.WriteLine("Van e benne Bear 650: " + statisztika.Contains("Bear 650"));
+            Console.WriteLine("Legöregebb motor:" + statisztika.OLdest());
+            Console.WriteLine("yamaha motorok ára egyben: " +statisztika.SumBasedOnBrand("Aprilia", "motors.txt") + "EUR");
+            statisztika.Sort();
+        
         }
     }
 }
